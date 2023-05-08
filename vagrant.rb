b4b27029c2b90e1e52f7e2e8f7b9049026189b8b
Sadiqul Islam <80273635+sadiq-simpletire@users.noreply.github.com>
2023-05-09 01:29:15 +0600

    For vagrant 2.2.6
    brew install --cask ...

---
+++ vagrant.rb
@@ -0,0 +1,21 @@
+cask 'vagrant' do
+  version '2.2.6'
+  sha256 'ff027c8e9dd8a9a24f391aa94fc06e798b8451d2661b56c5ced6235006feea54'
+
+  # hashicorp.com/vagrant was verified as official when first introduced to the cask
+  url "https://releases.hashicorp.com/vagrant/#{version}/vagrant_#{version}_x86_64.dmg"
+  appcast 'https://github.com/hashicorp/vagrant/releases.atom'
+  name 'Vagrant'
+  homepage 'https://www.vagrantup.com/'
+
+  pkg 'vagrant.pkg'
+
+  uninstall script:  {
+                       executable: 'uninstall.tool',
+                       input:      ['Yes'],
+                       sudo:       true,
+                     },
+            pkgutil: 'com.vagrant.vagrant'
+
+  zap trash: '~/.vagrant.d'
+end
