dd92f19769b40b5ac3c3a29d05db855f5aad9920
Sadiqul Islam <80273635+sadiq-simpletire@users.noreply.github.com>
2023-05-09 01:36:28 +0600

    vagrant 2.2.7

---
+++ 2.2.7/vagrant.rb
@@ -0,0 +1,21 @@
+cask 'vagrant' do
+  version '2.2.7'
+  sha256 '7d6451e80da0f8243ca9c0ff7299ef5ab106247ff41a2d296e717647d9ae6193'
+
+  # hashicorp.com/vagrant was verified as official when first introduced to the cask
+  url "https://releases.hashicorp.com/vagrant/#{version}/vagrant_#{version}_x86_64.dmg"
+  appcast 'https://github.com/hashicorp/vagrant/releases.atom'
+  name 'Vagrant'
+  homepage 'https://www.vagrantup.com/'
+
+  pkg 'vagrant.pkg'
+
+  uninstall script:  {
+                       executable: 'uninstall.tool',
+                       input:      ['Yes'],
+                       sudo:       true,
+                     },
+            pkgutil: 'com.vagrant.vagrant'
+
+  zap trash: '~/.vagrant.d'
+end
